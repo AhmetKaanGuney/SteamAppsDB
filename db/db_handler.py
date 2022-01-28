5e2a883346b63406d69d0a58ed9f91f329facc42
Ahmet Kaan Guney <68081622+AhmetKaanGuney@users.noreply.github.com>
2022-01-28 12:46:27 +0300

    Added error handling when requesting from an API

--- db/db_handler.py
+++ db/db_handler.py
@@ -3,7 +3,13 @@ import sqlite3
 import time
 
 import requests
+from db_errors import (
+    RequestTimeoutError, RequestFailedError,
+    UnauthorizedError, ForbiddenError,
+    NotFoundError, ServerError
+    )
 
+FAIL_API = "https://store.steampowered.com/api/appdetails/?appids=360032"
 
 # Get app list from Steam
 # For each app:
@@ -15,7 +21,7 @@ import requests
 #         record to db
 applist_fetched = True
 
-# Time to wait in between request in ms 
+# Time to wait in between request in ms
 STEAM_WAIT_DURATION = 1
 STEAMSPY_WAIT_DURATION = 1
 
@@ -28,80 +34,138 @@ STEAM_APP_DETAILS_API_BASE = "https://store.steampowered.com/api/appdetails/?app
 STEAMSPY_APP_DETAILS_API_BASE = "https://steamspy.com/api.php?request=appdetails&appid="
 
 def main():
-    # Get App List from Steam
+    # Get App List from Steam and Save It
     if applist_fetched is False:
-        fetch_applist(APPLIST_API, APPLIST_FILE, applist_fetched)
-        # TODO write data (make a function)
-    # with open(APPLIST_FILE, "r") as f:
-    #     applist = json.load(f)["applist"]
-    
-    # clear_file_content(APPLIST_FILTERED_FILE)
-    # app_details = get_app_details_from_steamspy(applist[:10], STEAMSPY_APP_DETAILS_API_BASE, STEAMSPY_WAIT_DURATION)
+        try:
+            applist = fetch_applist(APPLIST_API, APPLIST_FILE)
+        except RequestTimeoutError:
+            print("!!! ERROR !!!")
+            print("Cannot fetch applist because request timed out. Will wait 5 minutes.")
+            time.sleep(60 * 5)
+            applist = fetch_applist(APPLIST_API, APPLIST_FILE)
 
-    # # Append to filtered list
-    # with open(APPLIST_FILTERED_FILE, "a") as f:
-    #     json.dump(app_details, f, indent=2)
+        # Save to File
+        write_to_json(applist, APPLIST_FILE)
 
+    # Open App List File
+    with open(APPLIST_FILE, "r") as f:
+        applist = json.load(f)["applist"]
 
+    # Get App details For each App
+    limited_applist = applist[:10]
+    for app in limited_applist:
+        appid = app["appid"]
+        # Fetch from SteamSpy
+        resp_from_steamspy = fetch(STEAMSPY_APP_DETAILS_API_BASE + str(app["appid"]))
 
-def fetch_applist(api, applist_file, applist_fetched: bool):
-    if applist_fetched is False:
-        response = requests.get(api)
+        # Check minimum owner count
+        min_owner_count = get_min_owner_count(resp_from_steamspy)
 
-        if response.status_code == 200:
-            applist = response.json()["applist"]["apps"]
-            data = {"applist": []}
-            # Save each app that has a name
-            for app in applist:
-                if not app["name"]:
-                    continue
-                else:
-                    data["applist"].append(
-                        {
-                            "appid": app["appid"],
-                            "name": app["name"]
-                        }
-                    )
-            with open(applist_file, "w") as f:
-                json.dump(data, f)
+        if min_owner_count > 1_000_000:
+            continue
         else:
-            print("RequestFailed: ", response.status_code)
-        applist_fetched = True
+            app_details_from_steamspy = {
+                "owners": resp_from_steamspy["owners"],
+                "price": resp_from_steamspy["price"],
+                "positive_reviews": resp_from_steamspy["positive"],
+                "negative_reviews": resp_from_steamspy["negative"],
+                "tags": resp_from_steamspy["tags"]
+            }
+            app.update(app_details_from_steamspy)
+
+            # Save App to DB
+            with open("./app_details.json") as f:
+                app_details = json.load(f)
+                app_details["apps"].append(app)
+
+            write_to_json(app_details, "./app_details.json", indent=2)
+
+            # Get App from db
+            with open("./app_details.json") as f:
+                apps = json.load(f)["apps"]
+
+            app = [i for i in apps if i["appid"] == appid][0]
+
+            print(app)
+            # TODO get details from Steam
 
 
-def get_app_details_from_steamspy(applist: [], steamspy_api_base: str, wait_duration: int):
-    process = ""
-    data = {"applist": []}
 
-    print("Getting app details:")
 
-    for app in applist:
-        response = requests.get(steamspy_api_base + str(app["appid"]))
-        if response.status_code == 200:
-            response_data = response.json()
-            data["applist"].append(
+
+
+def fetch_applist(api: str):
+    """Steam's return format: {'applist': [{appid: int, name: str}]}"""
+    # TODO record fetch date
+    print("fetch_applist()")
+
+    response_json = fetch(api)
+
+    applist = {"applist": []}
+    # Save each app that has a name
+    for app in response_json["applist"]:
+        if app["name"]:
+            applist["applist"].append(
                 {
-                "appid": app["appid"],
-                "owners": response_data["owners"],
-                "price": response_data["price"],
-                "positive_reviews": response_data["positive"],
-                "negative_reviews": response_data["negative"],
-                "tags": response_data["tags"]
+                    "appid": app["appid"],
+                    "name": app["name"]
                 }
             )
-        else:
-            print("Request Failed: ", response.status_code)
-            print("App ID: ", app["appid"])
-            process += "#"
-            continue
-        time.sleep(wait_duration)
-        process += "#"
-        print(process, end="\r")
-    
-    print("Finished!")
-    return data
 
-     
+    print("TODO: Record that applist is fecthed! So it doesn't always fecthes from steam")
+    return applist
+
+
+def fetch(api: str) -> dict:
+    """Makes a request to an API and returns JSON. If fails will raise Exeception."""
+    response = request_from(api)
+
+    if response.status_code == 200:
+        return response.json()
+    elif response.status_code == 500:   # server error
+        raise ServerError(api)
+    elif response.status_code == 401:
+        raise UnauthorizedError(api)
+    elif response.status_code == 403:
+        raise ForbiddenError(api)
+    elif response.status_code == 404:
+        raise NotFoundError(api)
+    else:
+        raise RequestFailedError(response.status_code)
+
+
+def request_from(api: str, timeout=1):
+    """Tries 3 times before raising TimeoutError"""
+    attempt = 1
+    while attempt <= 3:
+        try:
+            response = requests.get(api, timeout=timeout)
+            return response
+        except requests.Timeout:
+            print("Request Timed Out:", api)
+            print("Attempt:", attempt)
+            time.sleep(10)
+            attempt += 1
+
+    raise RequestTimeoutError(api)
+
+
+def get_min_owner_count(app_details: dict) -> int:
+    """Returns minimum owner count"""
+    # owners example: "10,000 .. 20,000"
+    min_owners_raw = app_details["owners"].split("..")[0].strip()
+    min_owners_str = ""
+    for i in min_owners_raw.split(","):
+        min_owners_str += i
+
+    return int(min_owners_str)
+
+
+def write_to_json(data: any, file_path: str, indent=0):
+    with open(file_path, "w") as f:
+        json.dump(data, f, indent=indent)
+
+
 def clear_file_content(file_path: str):
     with open(file_path, "w") as f:
         f.write("")
@@ -110,7 +174,7 @@ def clear_file_content(file_path: str):
 def get_app_count():
     with open(APPLIST_FILE, "r") as f:
         applist = json.load(f)["applist"]
-    
+
     return len(applist)
 
 
