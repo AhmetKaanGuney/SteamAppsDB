160ddfb12aad23cc246e8cd8580ae276de25605c
Ahmet Kaan Guney <68081622+AhmetKaanGuney@users.noreply.github.com>
2022-02-12 14:30:50 +0300

    Changed path of venv activate script in setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
 Set-Alias -Name "sqlite3" -Value "C:/SQLite3/sqlite3.exe" -Option constant -Scope global
-C:\Users\ahmet\VsCodeProjects\SteamAppsDB\venv\Scripts\Activate.ps1
+.\venv\Scripts\Activate.ps1
 $env:FLASK_ENV = "development"
 # python app.py
