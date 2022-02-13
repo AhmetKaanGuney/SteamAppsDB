38e3286ae506d57a70aeaeb13c69a5510c467f77
Ahmet Kaan Guney <68081622+AhmetKaanGuney@users.noreply.github.com>
2022-02-13 11:09:03 +0300

    Added venv feature to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from db.update_logger import DEFAULT_LOG
 current_dir = os.path.dirname(__file__)
 env_path = os.path.join(current_dir, ".env")
 update_log_path = os.path.join(current_dir, "db/update_log.json")
+venv_path = os.path.join(current_dir, "venv")
 
 env_content = """\
 SECRET_KEY =
@@ -42,5 +43,22 @@ if __name__ == "__main__":
             print("Creating update_log file...")
             json.dump(DEFAULT_LOG, log_file, indent=2)
     print("---")
+
+    print("Checking if venv exists...")
+    if os.path.exists(venv_path):
+        print("venv already exists!\nResuming...")
+    else:
+        print("Creating venv...")
+        os.system("python -m venv venv")
+
+        print("---")
+
+        print("Activating venv...")
+        os.system("venv.ps1")
+
+        print("---")
+
+        print("Installing requirements...")
+        os.system("pip install -r requirements.txt")
 
     print("Setup succesfull!\n")
