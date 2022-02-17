9382742f2851a0327eaa0aa2bce89b6f6bd57f16
Ahmet Kaan Guney <68081622+AhmetKaanGuney@users.noreply.github.com>
2022-02-17 20:39:36 +0300

    Added freeze argument to diagnostics.py.

--- db/diagnostics.py
+++ db/diagnostics.py
@@ -1,11 +1,49 @@
+"""Diagnostics for apps.db"""
+
+
 if __name__ == "__main__":
+    import os
+    import sys
+    import json
+
     from database import (
         APPS_DB_PATH, Connection,
         get_failed_requests, get_non_game_apps
     )
 
+    current_dir = os.path.dirname(__file__)
+    diagnostic_folder = "diagnostics"
+    FAILED_REQUESTS_PATH = os.path.join(current_dir, diagnostic_folder, "failed_request.json")
+    NON_GAME_APPS_PATH = os.path.join(current_dir, diagnostic_folder, "non_game_apps.json")
+
+    args = sys.argv
+    write_to_json = False
+
+    if len(args) == 2:
+        if args[1] == "freeze":
+            write_to_json = True
+
+    if write_to_json is True:
+        os.mkdir(os.path.join(current_dir, diagnostic_folder))
+        with Connection(APPS_DB_PATH) as db:
+            print("Loading failed requests...")
+            failed_requests: list[dict] = get_failed_requests("", db)
+            print("Loading non-game apps...")
+            non_game_apps: list[int] = get_non_game_apps(db)
+
+        print("Writing failed reqeusts...")
+        with open(FAILED_REQUESTS_PATH, "w") as failed_file:
+            json.dump(failed_requests, failed_file)
+
+        print("Writing non=game apps...")
+        with open(NON_GAME_APPS_PATH, "w") as non_game_file:
+            json.dump(NON_GAME_APPS_PATH, non_game_file)
+
+        exit(0)
+
+
     filter_failed = ""
-    filter_failed = "WHERE cause != 'failed'"
+    filter_failed = "WHERE cause == 'failed'"
     with Connection(APPS_DB_PATH) as db:
         failed_requests = db.execute(f"""
             SELECT app_id, api_provider, cause, status_code
