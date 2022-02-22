c27864c7ab7a6b575fe4c01eb7d168e786e073e9
server <steamappsdb@gmail.com>
2022-02-22 07:32:37 +0000

    Fixed config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-import json
+import os
+from dotenv import dotenv_values
 
-with open("../env.json") as f:
-    ENV = json.load(f)
+current_dir = os.path.dirname(__file__)
+# Config
+env = dotenv_values(os.path.join(current_dir, ".env"))
 
-SECRET_KEY = ENV["SECRET_KEY"]
+SECRET_KEY = env["SECRET_KEY"]
